598c88eb76fe85de899b93281224fdd66b99b6ef
Hancer <hancermercedes@gmail.com>
2023-02-20 16:42:53 -0400

    Adding methods getall and getpage to the example.

--- Program.cs
+++ Program.cs
@@ -1,33 +1,7 @@
-﻿using Rvn.Ch02;
+﻿using Raven.Client.Documents;
+using Rvn.Ch02;
+using System.Runtime.CompilerServices;
 using static System.Console;
-
-//using (var session = DocumentStoreHolder.Store.OpenSession())
-//{
-//    var task = new ToDoTask
-//    {
-//        DueDate = DateTime.Today.AddDays(1),
-//        Task = "Buy milk"
-
-//    };
-//    session.Store(task);
-//    session.SaveChanges();
-//}
-
-//using (var session = DocumentStoreHolder._store.OpenSession())
-//{
-//    var task = new ToDoTask
-//    {
-//        DueDate = DateTime.Today.AddDays(2),
-//        Task = "Program the session page.",
-//        Completed = false,
-//    };
-
-//    session.Store(task);
-//    session.SaveChanges();
-
-//}
-
-
 int opc = 0;
 var Tasks = new ToDoTask();
 WriteLine("Orgnazing your days app..");
@@ -38,6 +12,8 @@ do
     WriteLine("Opction 1 : create a new task.");
     WriteLine("Opction 2 : get by Id to complete.");
     WriteLine("Opction 3 : delete a task.");
+    WriteLine("Opction 4 : get all the task. ");
+    WriteLine("Opction 5 : get page and records per page.");
     opc = int.Parse(Console.ReadLine()!);
     switch (opc)
     {
@@ -49,8 +25,6 @@ do
                 WriteLine("Date for the task? ");
                 var date = Convert.ToDateTime(ReadLine());
 
-                
-
                 Tasks.Task = task;
                 Tasks.DueDate = date;
                 using (var session = DocumentStoreHolder._store.OpenSession())
@@ -60,7 +34,7 @@ do
                 }
             }
             break;
-            case 2:
+      case 2:
             {
                 Console.WriteLine("Write the id: ");
                 var id = Console.ReadLine()!;
@@ -72,24 +46,63 @@ do
                     var task = session.Load<ToDoTask>(Tasks.Id);
                     task.Completed = true;
                     session.SaveChanges();
-                    Console.WriteLine(task.Task);
+                    Console.WriteLine($"the task: {task.Id}\t{task.Task}\t{task.Completed}\t{task.DueDate} was completed successfully.");
                 }
                 break;
             }
-        case 3:
-            {
-                Console.WriteLine("Write the id: ");
-                var id = Console.ReadLine()!;
-                Tasks.Id = id;
-
-                using (var session = DocumentStoreHolder._store.OpenSession())
+      case 3:
                 {
+                    Console.WriteLine("Write the id: ");
+                    var id = Console.ReadLine()!;
+                    Tasks.Id = id;
 
-                    session.Delete(Tasks.Id);
-                    session.SaveChanges();
-                    WriteLine($"The task with id:{Tasks.Id} was deleted successfuly.");
+                    using (var session = DocumentStoreHolder._store.OpenSession())
+                    {
+
+                        session.Delete(Tasks.Id);
+                        session.SaveChanges();
+                        WriteLine($"The task with id:{Tasks.Id} was deleted successfuly.");
+                    }
+
                 }
+                break;
+      case 4:
+            {
+                WriteLine("List of task..");
+                using (var session = DocumentStoreHolder._store.OpenAsyncSession())
+                {
+                    var tasks = await session.Query<ToDoTask>().ToListAsync();
 
+                    foreach (var task in tasks)
+                    {
+                        WriteLine($" Task: {task.Id} {task.Task} {task.Completed} {task.DueDate.ToShortDateString()}");
+                    }
+                }
+            }
+            break;
+
+        case 5:
+            {
+                WriteLine("Paging the result.");
+                WriteLine("Page number: ");
+                var pageNumber = int.Parse(ReadLine()!);
+                WriteLine("Page size: ");
+                var pageSize = int.Parse(ReadLine()!);
+                var skip = (pageNumber - 1) * pageSize;
+                var take = pageSize;
+                using (var session = DocumentStoreHolder._store.OpenAsyncSession())
+                {
+                   
+                    var tasks = await session.Query<ToDoTask>()
+                        .Skip(skip)
+                        .Take(take)
+                        .ToListAsync();
+
+                    foreach (var task in tasks)
+                    {
+                        WriteLine($" Task: {task.Id} {task.Task} {task.Completed} {task.DueDate.ToShortDateString()}");
+                    }
+                }
             }
             break;
 
@@ -98,7 +111,7 @@ do
 
 
 }
-while (opc != 5);
+while (opc != 100);
 {
     Console.ForegroundColor = ConsoleColor.Yellow;
     Console.WriteLine("Thanks for comming..");     
