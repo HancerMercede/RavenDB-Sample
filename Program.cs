9041586b145ff170ea59eaf5d948063bfdcd4846
Hancer <hancermercedes@gmail.com>
2023-02-20 22:53:08 -0400

    Adding global usings

--- Program.cs
+++ Program.cs
@@ -1,24 +1,27 @@
-﻿using Raven.Client.Documents;
-using Rvn.Ch02;
-using System.Runtime.CompilerServices;
-using static System.Console;
+﻿
+
 int opc = 0;
 var Tasks = new ToDoTask();
-WriteLine("Orgnazing your days app..");
+
+WriteLine("=======================");
+WriteLine("Task Manager.");
 do
 {
-
+    WriteLine("=======================");
     WriteLine("Select your opctions.");
     WriteLine("Opction 1 : create a new task.");
     WriteLine("Opction 2 : get by Id to complete.");
     WriteLine("Opction 3 : delete a task.");
-    WriteLine("Opction 4 : get all the task. ");
+    WriteLine("Opction 4 : get all the pending tasks. ");
     WriteLine("Opction 5 : get page and records per page.");
+    WriteLine("Opction 100: Exit.");
+
     opc = int.Parse(Console.ReadLine()!);
     switch (opc)
     {
         case 1:
             {
+
                 Write("Write your task: ");
                 var task = Console.ReadLine();
 
@@ -46,14 +49,15 @@ do
                     var task = session.Load<ToDoTask>(Tasks.Id);
                     task.Completed = true;
                     session.SaveChanges();
-                    Console.WriteLine($"the task: {task.Id}\t{task.Task}\t{task.Completed}\t{task.DueDate} was completed successfully.");
+                    ForegroundColor = ConsoleColor.Green;
+                    WriteLine($"the task: {task.Id}\t{task.Task}\t{task.Completed}\t{task.DueDate} was completed successfully.");
                 }
                 break;
             }
       case 3:
                 {
-                    Console.WriteLine("Write the id: ");
-                    var id = Console.ReadLine()!;
+                    WriteLine("Write the id: ");
+                    var id = ReadLine()!;
                     Tasks.Id = id;
 
                     using (var session = DocumentStoreHolder._store.OpenSession())
@@ -61,6 +65,7 @@ do
 
                         session.Delete(Tasks.Id);
                         session.SaveChanges();
+                        ForegroundColor = ConsoleColor.DarkRed;
                         WriteLine($"The task with id:{Tasks.Id} was deleted successfuly.");
                     }
 
@@ -68,10 +73,12 @@ do
                 break;
       case 4:
             {
-                WriteLine("List of task..");
+                WriteLine("Loading the tasks..");
                 using (var session = DocumentStoreHolder._store.OpenAsyncSession())
                 {
-                    var tasks = await session.Query<ToDoTask>().ToListAsync();
+                    var tasks = await session.Query<ToDoTask>()
+                        .Where(t=>t.Completed == true)
+                        .ToListAsync();
 
                     foreach (var task in tasks)
                     {
@@ -86,21 +93,26 @@ do
                 WriteLine("Paging the result.");
                 WriteLine("Page number: ");
                 var pageNumber = int.Parse(ReadLine()!);
+
                 WriteLine("Page size: ");
                 var pageSize = int.Parse(ReadLine()!);
+
                 var skip = (pageNumber - 1) * pageSize;
                 var take = pageSize;
+
                 using (var session = DocumentStoreHolder._store.OpenAsyncSession())
                 {
                    
                     var tasks = await session.Query<ToDoTask>()
+                        .Statistics(out QueryStatistics stats)
                         .Skip(skip)
                         .Take(take)
                         .ToListAsync();
 
                     foreach (var task in tasks)
                     {
-                        WriteLine($" Task: {task.Id} {task.Task} {task.Completed} {task.DueDate.ToShortDateString()}");
+                        WriteLine($" Task: {task.Id} {task.Task} {task.Completed} {task.DueDate.ToShortDateString()} of {stats.TotalResults}");
+                        WriteLine($"{pageNumber + 1} to {pageSize} of {stats.TotalResults}");
                     }
                 }
             }
---
+++ globals.cs
@@ -0,0 +1,4 @@
+﻿global using Raven.Client.Documents;
+global using Raven.Client.Documents.Session;
+global using Rvn.Ch02;
+global using static System.Console;
